39f8816168c4772ca8b91db800a1bac1460b49ca
mohanavamsi0614 <mohanavamsi14@gmail.com>
2024-12-19 23:16:26 +0530

    fo

--- Whac-a-Mole/app.js
+++ Whac-a-Mole/app.js
@@ -1,46 +1,48 @@
 const cells = document.querySelectorAll(".cell");
-const time = document.querySelector(".time");
-const scoreDisplay = document.querySelector(".score-display");
+    const time = document.querySelector(".time");
+    const scoreDisplay = document.querySelector(".score-display");
 
-let sec = 0;
-let score = 0;
-let ran=-1
-const kalam = setInterval(() => {
-  if (sec === 10) {
-    clearInterval(kalam);
-    clearInterval(ply);
-    scoreDisplay.innerHTML = `Final Score: ${score}`;
-  }
+    let sec = 0;
+    let score = 0;
+    let ran = -1;
 
-  time.innerHTML = `${sec}`;
-  sec++;
+    const kalam = setInterval(() => {
+      if (sec === 10) {
+        clearInterval(kalam);
+        clearInterval(ply);
+        scoreDisplay.innerHTML = `Final Score: ${score}`;
+      }
 
-}, 1000);
+      time.innerHTML = `Time: ${sec}`;
+      sec++;
 
-const ply = setInterval(() => {
-  play();
-}, 1000);
+    }, 1000);
 
-cells.forEach((cell,i)=>{
-    cell.addEventListener("click",()=>{
-        if(ran==i){
-            score++
-            scoreDisplay.innerHTML = `Score: ${score}`;
+    const ply = setInterval(() => {
+      play();
+    }, 1000);
+
+    cells.forEach((cell, i) => {
+      cell.addEventListener("click", () => {
+        if (ran === i) {
+          score++;
+          scoreDisplay.innerHTML = `Score: ${score}`;
         }
-    })
-})
-function play() {
-  const img = document.createElement("img");
-  img.src = "./images.jpeg";
-  img.style.width = "100%";
-  img.style.height = "100%";
-  img.style.borderRadius = "10px";
+      });
+    });
 
-   ran = Math.floor(Math.random() * cells.length);
-  cells[ran].appendChild(img);
+    function play() {
+      const img = document.createElement("img");
+      img.src = "./images.jpeg";
+      img.style.width = "100%";
+      img.style.height = "100%";
+      img.style.borderRadius = "10px";
 
-  const randomTime = Math.floor(Math.random() * 500) + 500;
-  setTimeout(() => {
-    cells[ran].removeChild(img);
-  }, randomTime);
-}
+      ran = Math.floor(Math.random() * cells.length);
+      cells[ran].appendChild(img);
+
+      const randomTime = Math.floor(Math.random() * 500) + 500;
+      setTimeout(() => {
+        cells[ran].removeChild(img);
+      }, randomTime);
+    }
